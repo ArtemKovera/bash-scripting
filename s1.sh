62505db0e7c59ce6bb14d3c92202cf7f5735cb0a
Artem Kovera <artemkovera@gmail.com>
2021-06-05 20:34:48 +0300

    my first bash script for updating the system

---
+++ s1.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Script for updating the system 
+# Uses '--u' and '--uc' command-line arguments
+
+# Functions:
+
+# Updating the system
+update()
+{
+   echo "Starting updating the system..."
+   sudo apt update
+   sudo apt dist-upgrade -yy
+   echo "The system has been updated"
+}
+
+# Cleaning the system
+clean()
+{
+    echo "Removing unnecessary packages..."
+    sudo apt autoremove -yy
+    sudo apt autoclean
+    echo "Packages have been removed"
+}
+
+# Update is complete
+end()
+{
+   echo "Updating is complete!"
+   exit
+}
+
+
+
+# Execution:
+
+echo "Script for updating the system"
+
+# Update:
+
+if [  "$1" = "--u" ]
+then
+    update
+    end
+fi
+
+if [ "$1" = "--uc" ]
+then
+    update
+    clean
+    end
+fi
+
+# Checking for invalid argument
+if [ -n "$1" ]
+then
+     echo "Erro: Invalid argument: ${1} Try '--u' for updating or '--uc' for updating and cleaning"
+     exit 1
+fi
+
+# if the user hasn't entered any arguments
+echo "You have not privided any comman-line argument" 
+echo "Try '--u' for updating or '--uc' for updating and cleaning"

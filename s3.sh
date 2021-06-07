42dff7bc47e2cc1b22d64849eb0f5309a16e31c3
Artem Kovera <artemkovera@gmail.com>
2021-06-07 08:16:10 +0300

    script for comparing two strings

---
+++ s3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this little script is for comparing two strings
+
+echo "Enter 1st string"
+read str1
+
+echo "Enter 2nd string"
+read str2
+
+if [ "$str1" == "$str2" ]
+then
+    echo "match"
+else
+    echo "mismatch"
+fi
